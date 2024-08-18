04054389196d66aea1bc8a0f7d89a734e9b2a90e
Akula Sheshu <121094754+Shesh009@users.noreply.github.com>
2024-08-18 21:37:33 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
       >
         <img
-          src="../public/Leetcode_1.png"
+          src="../Leetcode_1.png"
           alt="Lee"
           className="header-image"
         />
